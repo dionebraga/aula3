04297af3e5c2260fa39d66a4d23c26127c276b5b
dionebraga <dionebraga.work@gmail.com>
2024-06-18 20:29:57 -0300

    Exercicio aula 3 FashionStyle

---
+++ FashionStyle.py
@@ -0,0 +1,10 @@
+# Solicita o valor da compra ao cliente
+valor_compra = float(input("Digite o valor total da sua compra: R$ "))
+
+# Verifica os critérios de desconto e imprime as mensagens correspondentes
+if valor_compra > 500:
+    print("PARABÉNS. VOCÊ GANHOU SUPER DESCONTO DE 30%")
+elif valor_compra >= 250:
+    print("PARABÉNS. VOCÊ GANHOU 10% DE DESCONTO, MAS PODE GANHAR 30% SE SUA COMPRA FOR ACIMA DE R$500,00")
+else:
+    print("POXA, FALTA POUCO PARA VOCÊ GANHAR 10% DE DESCONTO EM SUA COMPRA.")
